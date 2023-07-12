09ad02777438a6aaeca1c7f3b536229c30e8ee33
Suhas KV <suhaskv0111@gmail.com>
2023-07-12 12:21:44 +0530

    Init: main.rs clap features testing, Cargo.toml updated with dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+/*  waveplot  */
+
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Takes the VCD file
+    #[arg(short, long, value_name = "FILE")]
+    vcd: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(vcd_file) = cli.vcd.as_deref() {
+        println!("Value change dump file: {}", vcd_file.display());
+    }
 }
